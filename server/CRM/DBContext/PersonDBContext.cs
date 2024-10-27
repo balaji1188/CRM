dc58b6866c2783434c2b8e74233589c59f292c95
abalaji-devloper <77772769+abalaji-devloper@users.noreply.github.com>
2024-10-27 18:30:40 +0530

    Added new Server code

---
+++ server/CRM/DBContext/PersonDBContext.cs
@@ -0,0 +1,14 @@
+﻿using CRM.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.DBContext
+{
+    public class PersonDBContext : DbContext
+    {
+        public PersonDBContext(DbContextOptions<PersonDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> persons { get; set; }
+    }
+}
